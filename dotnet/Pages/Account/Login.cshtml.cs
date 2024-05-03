992c051c134565ca8a4268c15545735b92cad4eb
Vitaliy <salinskiyvitaliy@gmail.com>
2024-05-04 00:54:57 +0200

    Presistent cookie

--- dotnet/Pages/Account/Login.cshtml.cs
+++ dotnet/Pages/Account/Login.cshtml.cs
@@ -35,7 +35,12 @@ namespace dotnet.Pages.Account
 				var identity = new ClaimsIdentity(claims, "MyCookieAuth");
 				ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
 
-				await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+				var authProperties = new AuthenticationProperties
+				{
+					IsPersistent = Credential.RememberMe
+				};
+
+				await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal, authProperties);
 
 				return RedirectToPage("/Index");
 			};
@@ -54,5 +59,8 @@ namespace dotnet.Pages.Account
 		[Required]
 		[DataType(DataType.Password)]
 		public string Password { get; set; } = string.Empty;
+
+		[Display(Name = "Remember me")]
+		public bool RememberMe { get; set; }
 	}
 }
